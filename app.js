6d85a77acaf28c67f97800adf7ef1eb02241f46b
孙悟元 <sun@wuyuan.dev>
2025-06-08 16:38:17 +0800

    Update app.js

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ process.on('uncaughtException', function (err) {
 });
 const app = express();
 
-var corslist=["localhost","zerocat.houlangs.com","zerocat.wuyuan.dev","z.8r.ink",'zerocatdev.github.io','zeronext.wuyuan.dev','scratch.190823.xyz',"scratch-editor.192325.xyz"]
+var corslist=["localhost","zerocat.houlangs.com","zerocat.wuyuan.dev",'scratch.190823.xyz',"scratch-editor.192325.xyz","zerocat.dev"]
 
 // cors配置
 import cors from "cors";
